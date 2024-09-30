4f6e586f44b938452e299e7ee85c7a8f71ee4db1
VenkteshT <venkyr929@gmail.com>
2024-09-30 21:08:07 +0530

    added comment in bookcontext

--- src/contexts/BooksContext.jsx
+++ src/contexts/BooksContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from "react";
 
+// since the dummy book api having an issue with search param so i'm managaing the book state globaly to perform filtering manually
 export const BooksContext = createContext();
 
 export const BooksProvider = ({ children }) => {
